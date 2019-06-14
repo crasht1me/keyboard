81cf5044fbb64064a0288700221f3d9f82fc8e37
vs-slavchev <vs_slavchev2@abv.bg>
2019-06-15 00:53:41 +0200

    Add layout 2D matrix and macros

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -1,8 +1,29 @@
+#define KEY_A 0x04
+#define KEY_B 0x05
+#define KEY_C 0x06
+#define KEY_D 0x07
+#define KEY_E 0x08
+#define KEY_F 0x09
+#define KEY_G 0x0A
+#define KEY_H 0x0B
+#define KEY_I 0x0C
+#define KEY_J 0x0D
+#define KEY_K 0x0E
+#define KEY_L 0x0F
+#define KEY_SHIFT_L 0x81
+
 const byte NUM_ROWS = 4;
-const byte NUM_COLS = 12;
+const byte NUM_COLS = 2;
 
 byte row_pins[NUM_ROWS] = {A0, A1, A2, A3};
-byte col_pins[NUM_COLS] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 14, 15};
+byte col_pins[NUM_COLS] = {2, 3/*, 4, 5, 6, 7, 8, 9, 10, 16, 14, 15*/};
+
+byte layout[NUM_ROWS][NUM_COLS] = {
+  {KEY_A, KEY_B},
+  {KEY_C, KEY_D},
+  {KEY_E, KEY_F},
+  {KEY_G, KEY_H}
+};
 
 
 void setup() {
@@ -11,8 +32,9 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  // reinitialise the column pins to allow sharing with other hardware
   for (byte col = 0; col < NUM_COLS; col++) {
-    pinMode(col_pins[col], INPUT_PULLUP);  
+    pinMode(col_pins[col], INPUT_PULLUP);
   }
 
   for (byte row = 0; row < NUM_ROWS; row++) {
@@ -23,7 +45,8 @@ void loop() {
       // reading low at the column means key is pressed
       bool is_pressed = !digitalRead(col_pins[col]);
       if (is_pressed) {
-        Serial.println("pressed: R" + String(row) + " C" + String(col));
+        //Serial.println("pressed: R" + String(row) + " C" + String(col));
+        Serial.println("pressed keycode: " + String(layout[row][col]));
       }
     }
     // return the row to high
